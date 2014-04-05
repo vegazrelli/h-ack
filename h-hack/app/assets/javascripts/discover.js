50a96b6c818af94cb91525cfb9dfb47b1d48c9a4
Pette <pettenon.daniel@gmail.com>
2014-04-06 01:03:23 +0200

    many images

--- h-hack/app/assets/javascripts/discover.js
+++ h-hack/app/assets/javascripts/discover.js
@@ -1,17 +1,142 @@
+var results = {
+  parigi: [
+    { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "0", fontSize: "1.1em" },
+    { imgSrc: "assets/parigi/1.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "17%", fontSize: "1em" },
+    { imgSrc: "assets/parigi/2.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "32.5%", fontSize: "0.65em" },
+    { imgSrc: "assets/parigi/6.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "43.5%", fontSize: "0.65em" },
+    { imgSrc: "assets/parigi/3.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "54%", fontSize: "0.65em" },
+    { imgSrc: "assets/parigi/4.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "67%", fontSize: "1em" },
+    { imgSrc: "assets/parigi/5.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "82%", fontSize: "1.1em" },
+    // { imgSrc: "assets/parigi/7.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "10%", fontSize: "1em" },
+    // { imgSrc: "assets/parigi/8.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "10%", fontSize: "1em" },
+    // { imgSrc: "assets/parigi/9.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    // { imgSrc: "assets/parigi/10.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    // { imgSrc: "assets/parigi/11.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    // { imgSrc: "assets/parigi/12.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+  ],
+  londra: [
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "0", fontSize: "1.1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "37%", left: "17%", fontSize: "1.2em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "34%", left: "31%", fontSize: "0.8em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "60%", left: "43%", fontSize: "1.em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "58%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "4%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+  ],
+  defaultResults: [
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "0", fontSize: "2em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "37%", left: "17%", fontSize: "1.2em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "34%", left: "31%", fontSize: "0.8em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "60%", left: "43%", fontSize: "1.em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "58%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "4%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+  ],
+};
+
+var allKeywords = [];
+
+
 $(document).on("click", ".keyword .remove_keyboard",function(e){
   e.preventDefault();
   var target = $(e.target);
 
-  target.closest("li").animate({opacity: 0}, function() {
+  target.closest("li").first().animate({opacity: 0}, function() {
     $(this).slideUp();
+    hideResults();
+    setTimeout(function() {
+      allKeywords.pop();
+      $(".results").html("");
+      showResults(allKeywords[allKeywords.length-1]);
+      console.log("qualcosa");
+    }, 500);
+
   });
 });
 
-var startAnimation = true;
 $("#search").on("change", function() {
-  if (startAnimation) {
-    console.log("mai");
-    
+  var keywordContent = $(this).val();
+  allKeywords.push(keywordContent);
+  if (keywordContent !== "") {
+    var keyword = $("<span class='keyword'># " + $(this).val() + "<a href='#'' class='remove_keyboard'>×</a></span>");
+    keyword.css({position: "absolute",top: "5px", opacity:"0"});
+    hideResults();
+    $(this).parent().append(keyword);
+    keyword.animate({opacity: 1}, function() {
+      var fromLeft = keyword.offset().left;
+      var fromTop = keyword.offset().top;
+      var toLeft = $("body").width() - keyword.width() - 35;
+      var toTop;
+      if($(".keywords").html().length < 5) {
+        toTop = 15;
+      } else {
+        toTop = $(".keywords li:last-child").offset().top  + keyword.outerHeight() + 5;
+      }
+      
+      keyword.css({position: "fixed", top: fromTop, left: fromLeft});
+      keyword.animate( {top: toTop, left: toLeft}, function() {
+        var positionedKeyword = $("<li><span class='keyword'># " + keywordContent + "<a href='#' class='remove_keyboard'>×</a></span></li>");
+        keyword.remove();
+        $(".keywords").append(positionedKeyword);
+          $(".results").html("");
+          showResults(keywordContent);
+      });
+    });
+
     $(this).val("");
   }
+});
+
+function hideResults() {
+  $(".results .result").animate({top: "100%", left: "50%"});
+}
+
+function showResults(keyword) {
+  var resultsFor = results[keyword];
+  if(typeof resultsFor === "undefined") {
+    console.log("miap");
+    resultsFor = results["defaultResults"];
+  }
+  var result;
+  for(var i = 0; i < resultsFor.length; i++) {
+    result = $("<div class='result'> <div class='photo'> <img src='"+ resultsFor[i]["imgSrc"] +"' </div> <div class='user_name'>" + resultsFor[i]["userName"] + "</div> <div class='caption'>" + resultsFor[i]["caption"] + "</div> </div>");
+    result.css({fontSize: resultsFor[i]["fontSize"]});
+    $(".results").append(result);
+    console.log("enter");
+    result.animate({top:  resultsFor[i]["top"], left:  resultsFor[i]["left"], fontSize: resultsFor[i]["font"]});
+  }
+}
+
+$(window).on("load", function(){
+  setTimeout(function() {
+    showResults();
+  }, 2000);
+
 });
--- h-hack/app/controllers/home_controller.rb
+++ h-hack/app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     respond_to do |format|
-      format.html { render layout: "discover"}
+      format.html { render layout: "discover" }
     end
   end
 end
