ae6ed03cbbf5502dba589f4d3d75db5b6cdee744
Pette <pettenon.daniel@gmail.com>
2014-04-05 17:41:19 +0200

    rails app crate

---
+++ h-hack/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HHack::Application.config.secret_token = 'e877cbf4c856e3f410b1cfa284cf25234b813f176ecf5eda770f99bc7cd263a539617d1cd4870c798069d2b68dcb68745b051aeb33d6745c03c3024521d669c0'
