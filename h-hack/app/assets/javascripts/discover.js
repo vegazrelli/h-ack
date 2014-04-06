2a0599044110ecc9980d71c59f965a2de6a282c6
Pette <pettenon.daniel@gmail.com>
2014-04-06 03:26:34 +0200

    fixed

--- h-hack/app/assets/javascripts/discover.js
+++ h-hack/app/assets/javascripts/discover.js
@@ -1,25 +1,25 @@
 var results = {
   parigi: [
-    { imgSrc: "assets/parigi/0.JPG", caption: "La città più bella del mondo", userName: "vegaz",top: "0%", left: "1em", fontSize: "1.5em" },
-    { imgSrc: "assets/parigi/1.JPG", caption: "Il viaggio della vita", userName: "vegaz",top: "0", left: "20em", fontSize: "1.3em" },
-    { imgSrc: "assets/parigi/2.JPG", caption: "Il viaggio più bello dell'estate!", userName: "vegaz",top: "0", left: "47.5em", fontSize: "1em" },
-    { imgSrc: "assets/parigi/6.JPG", caption: "Mi trasferirei immediatamente!", userName: "vegaz",top: "47em", left: "47em", fontSize: "1em" },
-    { imgSrc: "assets/parigi/3.JPG", caption: "Adoro i francesi", userName: "vegaz",top: "41em", left: "19.5em", fontSize: "0.72em" },
-    { imgSrc: "assets/parigi/4.JPG", caption: "E' una città così romantica..", userName: "vegaz",top: "20em", left: "47.5em", fontSize: "1em" },
-    { imgSrc: "assets/parigi/5.JPG", caption: "Che bei viali", userName: "vegaz",top: "0", left: "56em", fontSize: "1.35em" },
-    { imgSrc: "assets/parigi/7.JPG", caption: "Una grande città da vivere", userName: "vegaz",top: "41em", left: "2.2em", fontSize: "0.72em" },
-    { imgSrc: "assets/parigi/8.JPG", caption: "Ci tornerò", userName: "vegaz",top: "19.5em", left: "20em", fontSize: "1.3em" },
-    { imgSrc: "assets/parigi/9.JPG", caption: "A presto Parigi", userName: "vegaz",top: "0", left: "31.5em", fontSize: "2.05em" },
-    { imgSrc: "assets/parigi/10.JPG", caption: "Una città e un viaggio che mi rimarrà nel cuore!", userName: "vegaz",top: "30.5em", left: "36.5em", fontSize: "1.3em" },
-    { imgSrc: "assets/parigi/11.JPG", caption: "Da vivere", userName: "vegaz",top: "22.6em", left: "39.6em", fontSize: "1.75em" },
-    // { imgSrc: "assets/parigi/12.JPG", caption: "Un emozione unica.", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/parigi/0.JPG", caption: "La città più bella del mondo", userName: "Davide Valsecchi",top: "0%", left: "1em", fontSize: "1.5em" },
+    { imgSrc: "assets/parigi/1.JPG", caption: "Il viaggio della vita", userName: "Andrea Zanini",top: "0", left: "20em", fontSize: "1.3em" },
+    { imgSrc: "assets/parigi/2.JPG", caption: "Il viaggio più bello dell'estate!", userName: "Erica Riceputi",top: "0", left: "47.5em", fontSize: "1em" },
+    { imgSrc: "assets/parigi/6.JPG", caption: "Mi trasferirei immediatamente!", userName: "Giulia Stornante",top: "47em", left: "47em", fontSize: "1em" },
+    { imgSrc: "assets/parigi/3.JPG", caption: "Adoro i francesi", userName: "Giuseppe Rota",top: "41em", left: "19.5em", fontSize: "0.72em" },
+    { imgSrc: "assets/parigi/4.JPG", caption: "E' una città così romantica..", userName: "Andrea Maconi",top: "20em", left: "47.5em", fontSize: "1em" },
+    { imgSrc: "assets/parigi/5.JPG", caption: "Che bei viali", userName: "Il Bobe",top: "0", left: "56em", fontSize: "1.35em" },
+    { imgSrc: "assets/parigi/7.JPG", caption: "Una grande città da vivere", userName: "Martina Lodetti",top: "41em", left: "2.2em", fontSize: "0.72em" },
+    { imgSrc: "assets/parigi/8.JPG", caption: "Ci tornerò", userName: "Umberto Mazzotti",top: "19.5em", left: "20em", fontSize: "1.3em" },
+    { imgSrc: "assets/parigi/9.JPG", caption: "A presto Parigi", userName: "Michele Rota",top: "0", left: "31.5em", fontSize: "2.05em" },
+    { imgSrc: "assets/parigi/10.JPG", caption: "Una città e un viaggio che mi rimarrà nel cuore!", userName: "Riccardo Pressiani",top: "30.5em", left: "36.5em", fontSize: "1.3em" },
+    { imgSrc: "assets/parigi/11.JPG", caption: "Da vivere", userName: "Hackim Abdalla",top: "22.6em", left: "39.6em", fontSize: "1.75em" },
+    // { imgSrc: "assets/parigi/12.JPG", caption: "Un emozione unica.", userName: "Giorgia Previtali",top: "5%", left: "10%", fontSize: "1em" },
     // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
   ],
-  londra: [
+  defaultResults: [
     { imgSrc: "assets/GOPR0458.JPG", caption: "Che esperienza!", userName: "vegaz",top: "0", left: "1%", fontSize: "1.5em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "Una di quelle cose da NON PERDERE a Parigi!",userName: "vegaz",top: "0", left: "25%", fontSize: "1.3em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "Di sicuro nella mia TOP10!", userName: "vegaz",top: "34%", left: "31%", fontSize: "0.8em" },
@@ -39,20 +39,20 @@ var results = {
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
   ],
-  defaultResults: [
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata",userName: "vegaz",top: "0", left: "1%", fontSize: "1.3em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "25%", fontSize: "1.3em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "34%", left: "31%", fontSize: "0.8em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "60%", left: "43%", fontSize: "1.em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "58%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "4%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+  cibo: [
+    { imgSrc: "assets/GOPR0458.JPG", caption: "delizioso",userName: "Davide Valsecchi",top: "0", left: "1%", fontSize: "1.3em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "da gustare in giro", userName: "Andrea Zanini",top: "0", left: "25%", fontSize: "1.3em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "perfetto per una merenda a parigi", userName: "Erica Riceputi",top: "34%", left: "31%", fontSize: "0.8em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "così buono!!", userName: "Giulia Stornante",top: "60%", left: "43%", fontSize: "1.em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "mmm!", userName: "Giuseppe Rota",top: "5%", left: "58%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "non mi ha convinto", userName: "Andrea Maconi",top: "5%", left: "4%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "proprio no", userName: "Il Bobe",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "fantastico", userName: "Martina Lodetti",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "ho un palato sopraffino ma mi ha convinto!", userName: "Umberto Mazzotti",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "proprio no", userName: "Michele Rota",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "da provare assolutamente", userName: "Riccardo Pressiani",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "il sapore di parigi!", userName: "Hackim Abdalla",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "da gustare con una bella passeggiata", userName: "Giorgia Previtali",top: "5%", left: "10%", fontSize: "1em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
@@ -135,22 +135,44 @@ function showResults(keyword) {
 }
 
 $(window).on("load", function(){
+  var bodyWidth          = $("body").width();
+  var resultsContainer   = $(".results");
+
+  if(bodyWidth < 801) {
+    resultsContainer.css({fontSize: "0.5em"});
+  } else if (bodyWidth < 960) {
+    resultsContainer.css({fontSize: "0.6em"});
+  } else if (bodyWidth < 1153) {
+    resultsContainer.css({fontSize: "0.7em"});
+  } else if (bodyWidth < 1281) {
+    resultsContainer.css({fontSize: "0.8em"});
+  } else if (bodyWidth < 1361) {
+    resultsContainer.css({fontSize: "0.9em"});
+  } else {
+    resultsContainer.css({fontSize: "1em"});
+  }
   setTimeout(function() {
     showResults();
   }, 2000);
 
+
 });
 
-"La città più bella del mondo"
-"Il viaggio della vita"
-"Il viaggio più bello dell'estate!"
-"Mi trasferirei immediatamente!"
-"Adoro i francesi"
-"E' una città così romantica.."
-"Che bei viali"
-"Una grande città da vivere"
-"Ci tornerò"
-"A presto Parigi"
-"Una città e un viaggio che mi rimarrà nel cuore!"
-"Da vivere"
-"Un emozione unica."
+$(window).resize(function() {
+  var bodyWidth          = $("body").width();
+  var resultsContainer   = $(".results");
+
+  if(bodyWidth < 801) {
+    resultsContainer.css({fontSize: "0.5em"});
+  } else if (bodyWidth < 960) {
+    resultsContainer.css({fontSize: "0.6em"});
+  } else if (bodyWidth < 1153) {
+    resultsContainer.css({fontSize: "0.7em"});
+  } else if (bodyWidth < 1281) {
+    resultsContainer.css({fontSize: "0.8em"});
+  } else if (bodyWidth < 1361) {
+    resultsContainer.css({fontSize: "0.9em"});
+  } else {
+    resultsContainer.css({fontSize: "1em"});
+  }
+});
