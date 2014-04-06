847e5682577158b98d65f457787532e587eb4135
Pette <pettenon.daniel@gmail.com>
2014-04-06 02:19:42 +0200

    photos

--- h-hack/app/assets/javascripts/discover.js
+++ h-hack/app/assets/javascripts/discover.js
@@ -1,18 +1,18 @@
 var results = {
   parigi: [
-    { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "0", fontSize: "1.1em" },
-    { imgSrc: "assets/parigi/1.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "17%", fontSize: "1em" },
-    { imgSrc: "assets/parigi/2.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "32.5%", fontSize: "0.65em" },
-    { imgSrc: "assets/parigi/6.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "43.5%", fontSize: "0.65em" },
-    { imgSrc: "assets/parigi/3.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "54%", fontSize: "0.65em" },
-    { imgSrc: "assets/parigi/4.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "67%", fontSize: "1em" },
-    { imgSrc: "assets/parigi/5.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "82%", fontSize: "1.1em" },
-    // { imgSrc: "assets/parigi/7.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "10%", fontSize: "1em" },
-    // { imgSrc: "assets/parigi/8.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "10%", fontSize: "1em" },
-    // { imgSrc: "assets/parigi/9.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    // { imgSrc: "assets/parigi/10.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    // { imgSrc: "assets/parigi/11.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    // { imgSrc: "assets/parigi/12.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/parigi/0.JPG", caption: "La città più bella del mondo", userName: "vegaz",top: "0%", left: "1em", fontSize: "1.5em" },
+    { imgSrc: "assets/parigi/1.JPG", caption: "Il viaggio della vita", userName: "vegaz",top: "0", left: "20em", fontSize: "1.3em" },
+    { imgSrc: "assets/parigi/2.JPG", caption: "Il viaggio più bello dell'estate!", userName: "vegaz",top: "0", left: "47.5em", fontSize: "1em" },
+    { imgSrc: "assets/parigi/6.JPG", caption: "Mi trasferirei immediatamente!", userName: "vegaz",top: "47em", left: "47em", fontSize: "1em" },
+    { imgSrc: "assets/parigi/3.JPG", caption: "Adoro i francesi", userName: "vegaz",top: "41em", left: "19.5em", fontSize: "0.72em" },
+    { imgSrc: "assets/parigi/4.JPG", caption: "E' una città così romantica..", userName: "vegaz",top: "20em", left: "47.5em", fontSize: "1em" },
+    { imgSrc: "assets/parigi/5.JPG", caption: "Che bei viali", userName: "vegaz",top: "0", left: "56em", fontSize: "1.35em" },
+    { imgSrc: "assets/parigi/7.JPG", caption: "Una grande città da vivere", userName: "vegaz",top: "41em", left: "2.2em", fontSize: "0.72em" },
+    { imgSrc: "assets/parigi/8.JPG", caption: "Ci tornerò", userName: "vegaz",top: "19.5em", left: "20em", fontSize: "1.3em" },
+    { imgSrc: "assets/parigi/9.JPG", caption: "A presto Parigi", userName: "vegaz",top: "0", left: "31.5em", fontSize: "2.05em" },
+    { imgSrc: "assets/parigi/10.JPG", caption: "Una città e un viaggio che mi rimarrà nel cuore!", userName: "vegaz",top: "30.5em", left: "36.5em", fontSize: "1.3em" },
+    { imgSrc: "assets/parigi/11.JPG", caption: "Da vivere", userName: "vegaz",top: "22.6em", left: "39.6em", fontSize: "1.75em" },
+    // { imgSrc: "assets/parigi/12.JPG", caption: "Un emozione unica.", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
@@ -20,19 +20,19 @@ var results = {
     // { imgSrc: "assets/parigi/0.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
   ],
   londra: [
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "0", fontSize: "1.1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "37%", left: "17%", fontSize: "1.2em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "34%", left: "31%", fontSize: "0.8em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "60%", left: "43%", fontSize: "1.em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "58%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "4%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Che esperienza!", userName: "vegaz",top: "0", left: "1%", fontSize: "1.5em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Una di quelle cose da NON PERDERE a Parigi!",userName: "vegaz",top: "0", left: "25%", fontSize: "1.3em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Di sicuro nella mia TOP10!", userName: "vegaz",top: "34%", left: "31%", fontSize: "0.8em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Bello...", userName: "vegaz",top: "60%", left: "43%", fontSize: "1.em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Da vedere!", userName: "vegaz",top: "5%", left: "58%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Mmm.. non mi ha convinto..", userName: "vegaz",top: "5%", left: "4%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Perchè no, andateci..", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Fenomenale!", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Un classico, ma non perde il suo fascino..", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "THE BEST!", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Non potete perdervelo!", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La cosa migliore che ho trovato a Parigi", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "Bello, ma mi aspettavo di più", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
@@ -40,8 +40,8 @@ var results = {
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "10%", fontSize: "1em" },
   ],
   defaultResults: [
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "0", fontSize: "2em" },
-    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "37%", left: "17%", fontSize: "1.2em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata",userName: "vegaz",top: "0", left: "1%", fontSize: "1.3em" },
+    { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "0", left: "25%", fontSize: "1.3em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "34%", left: "31%", fontSize: "0.8em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "60%", left: "43%", fontSize: "1.em" },
     { imgSrc: "assets/GOPR0458.JPG", caption: "La mia scalata", userName: "vegaz",top: "5%", left: "58%", fontSize: "1em" },
@@ -95,7 +95,7 @@ $("#search").on("change", function() {
       var toLeft = $("body").width() - keyword.width() - 35;
       var toTop;
       if($(".keywords").html().length < 5) {
-        toTop = 15;
+        toTop = 65;
       } else {
         toTop = $(".keywords li:last-child").offset().top  + keyword.outerHeight() + 5;
       }
@@ -140,3 +140,17 @@ $(window).on("load", function(){
   }, 2000);
 
 });
+
+"La città più bella del mondo"
+"Il viaggio della vita"
+"Il viaggio più bello dell'estate!"
+"Mi trasferirei immediatamente!"
+"Adoro i francesi"
+"E' una città così romantica.."
+"Che bei viali"
+"Una grande città da vivere"
+"Ci tornerò"
+"A presto Parigi"
+"Una città e un viaggio che mi rimarrà nel cuore!"
+"Da vivere"
+"Un emozione unica."
